ed32332667c414ae49154cb1e81f59c5b26ddc81
Indra Rizky Gunawan <indra.rizky@bankmuamalat.co.id>
2024-12-14 14:11:06 +0700

    Update Telkomsel Program Page

--- src/components/Program/Telkomsel/index.js
+++ src/components/Program/Telkomsel/index.js
@@ -34,7 +34,7 @@ const TelkomselSection = () => {
       <ProgramWrapper>
         <ProgramIcon src={`/telkomsel/Mengapa DPLK Syariah Muamalat.png`} />
         <ProgramCard>
-          <BtnLink1 href="https://www.dplksyariahmuamalat.co.id/peserta/registrasi/" target="_blank">Daftar Sekarang</BtnLink1>
+          <BtnLink1 href="https://www.dplksyariahmuamalat.co.id/peserta/registrasi/">Daftar Sekarang</BtnLink1>
           <ProgramH2>*Masukkan kode <strong>TELKOMSEL</strong> pada kolom <br/> Nomor Referensi saat pendaftaran online</ProgramH2>
           <BtnLink2 href="https://linktr.ee/muamalatXtelkomsel" target="_blank">Panduan Informasi</BtnLink2>
           <BtnLinks to="#" onClick={handlePopupOpen}>Biaya Spesial</BtnLinks>
